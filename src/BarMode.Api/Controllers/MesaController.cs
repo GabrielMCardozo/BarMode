55fbe9c3cc89e9c82216e4a6fa50120dd763b764
GabrielVtex <gabriel.medeiros@vtex.com.br>
2014-03-20 07:51:12 -0300

    Refatorando

--- src/BarMode.Api/Controllers/MesaController.cs
+++ src/BarMode.Api/Controllers/MesaController.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
-using BarMode;
+
 namespace BarMode.Api.Controllers
 {
     public class MesaController : ApiController
     {
-        //public Guid Post(Mesa mesa)
-        //{
-        //    return Guid.NewGuid();
-        //}
+        public Guid Post(Mesa mesa)
+        {
+            return Guid.NewGuid();
+        }
     }
 }
