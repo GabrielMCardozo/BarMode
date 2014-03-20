e70a3ad10fa0766c24d7802c17fbfc4f6d39c99d
GabrielVtex <gabriel.medeiros@vtex.com.br>
2014-03-20 08:52:51 -0300

    Instalando o ravendb

--- src/BarMode.Api/Global.asax.cs
+++ src/BarMode.Api/Global.asax.cs
@@ -4,14 +4,58 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using Raven.Client;
+using Raven.Client.Document;
 
 namespace BarMode.Api
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        public static IDocumentStore DocumentStore { get; private set; }
+        
+        public WebApiApplication()
+        {
+            BeginRequest += Application_BeginRequest;
+            EndRequest += Application_EndRequest;
+        }
+
+        private void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpContext.Current.Items["CurrentRequestRavenSession"] = DocumentStore.OpenSession();
+        }
+
+        private void Application_EndRequest(object sender, EventArgs e)
+        {
+            using (var session = (IDocumentSession)HttpContext.Current.Items["CurrentRequestRavenSession"])
+            {
+                if (session == null)
+                    return;
+
+                if (Server.GetLastError() != null)
+                    return;
+
+                session.SaveChanges();
+            }
+        }
+
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            InitializeDocumentStore();
         }
+
+        private static void InitializeDocumentStore()
+        {
+            if (DocumentStore != null) return; // prevent misuse
+
+            DocumentStore = new DocumentStore
+            {
+                ConnectionStringName = "RAVENHQ_CONNECTION_STRING"
+            }.Initialize();
+
+        }
+
+
     }
 }
---
+++ src/BarMode.Api/Raven/RavenManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using Raven.Client;
+
+namespace BarMode.Api.Raven
+{
+    public static class RavenManager
+    {
+        public static IDocumentSession CurrentSession { get { return (IDocumentSession)HttpContext.Current.Items["CurrentRequestRavenSession"]; } }
+    }
+}
